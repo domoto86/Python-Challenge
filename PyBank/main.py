1c22e4d5e6c9fe566c568152cc9c4fd74658735c
domoto86 <122246053+domoto86@users.noreply.github.com>
2023-03-13 12:00:17 -0400

    Create main.py

---
+++ PyBank/main.py
@@ -0,0 +1,55 @@
+import csv
+
+total_months = 0
+pl_total = 0
+previous_month = 0
+profit_changes = []
+great_inc = ["", 0]
+great_dec = ["", 999999999]
+
+# Read CSV file
+with open('Resources/budget_data.csv') as csvfile:
+    file = csv.reader(csvfile, delimiter=',')
+    
+    next(file)
+    
+    for row in file:
+
+        total_months = total_months + 1
+
+        pl_total = pl_total + int(row[1])
+
+        this_month = int(row[1])
+
+        if total_months > 1:
+            profit_change = this_month - previous_month
+            profit_changes.append(profit_change)
+
+            if profit_change > great_inc[1]:
+                great_inc[0] = row[0]
+                great_inc[1] = profit_change
+
+            if profit_change < great_dec[1]:
+                great_dec[0] = row[0]
+                great_dec[1] = profit_change
+
+        previous_month = this_month
+
+average_change = sum(profit_changes) / len(profit_changes)
+
+print("PyBasnk Analysis")
+print("----------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${pl_total}")
+print(f"Average Change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits: {great_inc[0]} (${great_inc[1]})")
+print(f"Greatest Decrease in Profits: {great_dec[0]} (${great_dec[1]})")
+
+with open("Analysis/pybank_results.txt", "w") as text_file:
+    text_file.write("Financial Analysis\n")
+    text_file.write("----------------------------\n")
+    text_file.write(f"Total Months: {total_months}\n")
+    text_file.write(f"Total: ${pl_total}\n")
+    text_file.write(f"Average Change: ${average_change:.2f}\n")
+    text_file.write(f"Greatest Increase in Profits: {great_inc[0]} (${great_inc[1]})\n")
+    text_file.write(f"Greatest Decrease in Profits: {great_dec[0]} (${great_dec[1]})\n")
